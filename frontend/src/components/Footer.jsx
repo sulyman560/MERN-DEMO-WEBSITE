5d27b03a711845bf4e3c8d3455bdef077e607101
sulyman560 <sulyman560@gmail.com>
2026-03-06 21:37:30 +0600

    fix

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets';
 
 const Footer = () => {
 
@@ -20,10 +21,10 @@ const Footer = () => {
 
 
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/10">
             <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
                 <div>
-                    <img className="w-34 md:w-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
+                    <img className="w-34 md:w-32" src={assets.logo} alt="dummyLogoColored" />
                     <p className="max-w-[410px] mt-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
@@ -42,7 +43,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright 2025 © <a href="https://prebuiltui.com">PrebuiltUI</a> All Right Reserved.
+                Copyright 2025 © CoderSulyman All Right Reserved.
             </p>
         </div>
   )
